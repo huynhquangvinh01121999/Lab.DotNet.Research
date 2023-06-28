65c5ed82d00a5f30a32e3a89fd14af53e5e457a4
Huỳnh Quang Vinh <vinhhq@esuhai.com>
2023-06-28 17:12:08 +0700

    upload source worker

---
+++ Lab.WorkerService.Basic/Lab.WorkerService.Basic/Worker.cs
@@ -0,0 +1,69 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab.WorkerService.Basic
+{
+    public class Worker : BackgroundService
+    {
+        private int executionCount = 0;
+        private readonly ILogger<Worker> _logger;
+        private Timer _timer = null;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        // StartAsync is called before
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //{
+        //    while (!stoppingToken.IsCancellationRequested)
+        //    {
+        //        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        //        await Task.Delay(1000, stoppingToken);
+        //    }
+        //}
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoWork(object state)
+        {
+            var count = Interlocked.Increment(ref executionCount);
+
+            _logger.LogInformation(
+                "Timed Hosted Service is working. Count: {Count}", count);
+        }
+    }
+}
