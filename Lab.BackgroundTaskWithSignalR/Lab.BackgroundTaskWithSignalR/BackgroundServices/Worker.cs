cb01fc7ca80a1028da408376253d7cdaa9b7d9fb
Huỳnh Quang Vinh <vinhhq@esuhai.com>
2023-07-27 14:27:45 +0700

    add source Lab.BackgroundTaskWithSignalR

---
+++ Lab.BackgroundTaskWithSignalR/Lab.BackgroundTaskWithSignalR/BackgroundServices/Worker.cs
@@ -0,0 +1,48 @@
+﻿using Lab.BackgroundTaskWithSignalR.SignalR;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab.BackgroundTaskWithSignalR.BackgroundServices
+{
+    public class Worker : BackgroundService
+    {
+        private readonly IHubContext<SignalHub> _hubContext;
+        private Timer _timer;
+
+        public Worker(IHubContext<SignalHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Da kich hoat cac tac vu nen");
+
+            // Chạy tác vụ nền mỗi 5 giây
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            await Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            var message = $"Đây là thông báo từ server lúc {DateTime.Now}";
+            _hubContext.Clients.All.SendAsync("ReceiveNotification", message);
+
+            Console.WriteLine("Da gui notification cho client");
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Da dung cac tac vu nen");
+
+            // Dừng tác vụ nền khi dừng ứng dụng
+            _timer?.Change(Timeout.Infinite, 0);
+
+            await base.StopAsync(stoppingToken);
+        }
+    }
+}
---
+++ Lab.BackgroundTaskWithSignalR/Lab.BackgroundTaskWithSignalR/SignalR/SignalHub.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lab.BackgroundTaskWithSignalR.SignalR
+{
+    public class SignalHub : Hub
+    {
+        private static readonly Dictionary<string, List<string>> usersOnline = new Dictionary<string, List<string>>();
+
+        public override Task OnConnectedAsync()
+        {
+            lock (usersOnline)
+            {
+                var userId = Context.GetHttpContext().Request.Query["userId"].ToString();
+
+                if (usersOnline.ContainsKey(userId))
+                {
+                    usersOnline[userId].Add(Context.ConnectionId);
+                }
+                else
+                {
+                    usersOnline.Add(userId, new List<string> { Context.ConnectionId });
+                }
+            }
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            var userId = Context.GetHttpContext().Request.Query["userId"].ToString();
+
+            if (!string.IsNullOrEmpty(userId) && usersOnline.ContainsKey(userId) && usersOnline[userId].Contains(Context.ConnectionId))
+            {
+                usersOnline[userId].Remove(Context.ConnectionId);
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
